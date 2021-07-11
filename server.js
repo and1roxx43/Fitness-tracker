72ef937868458a6c7845655e8052279e0b6edfe8
andrew quenette <and1roxx43@gmail.com>
2021-07-11 23:48:29 +1000

    Modify Server.js

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const apiRoutes = require("./routes/api");
+const indexRoutes = require("./routes/index");
 
 const PORT = process.env.PORT || 3002;
 
+const app = express();
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
+{
+    useNewUrlParser: true
+});
+
+
+app.use(apiRoutes);
+app.use(indexRoutes);
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+});
