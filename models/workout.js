f6dd5f4fa5dce6e259624e7ad375f82a6bea22b2
andrew quenette <and1roxx43@gmail.com>
2021-07-11 22:24:35 +1000

    Added models

---
+++ models/workout.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema ({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercise: [
+        {
+    name: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    weight: {
+        type: Number,
+        required: false
+    },
+    reps: {
+        type: Number,
+        required: false
+    },
+    sets: {
+        type: Number,
+        required: false
+    },
+    distances: {
+        type: Number,
+        required: false
+    }
+}]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workoput;
--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3005;
+const PORT = process.env.PORT || 3002;
 
